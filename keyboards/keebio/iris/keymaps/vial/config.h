0723ac7b5c740d7ebcf4143eebf62e3a655909be
Alexis Delgado <alexis@98point6.com>
2023-03-12 20:57:03 -0700

    vial

--- keyboards/keebio/iris/keymaps/vial/config.h
+++ keyboards/keebio/iris/keymaps/vial/config.h
@@ -22,8 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Added by Brian C. Arnold to support Vial.
 #define VIAL_KEYBOARD_UID {0xBC, 0x99, 0xF2, 0x61, 0x45, 0x7C, 0xC8, 0xB9}
-#define VIAL_UNLOCK_COMBO_ROWS {0,9}
-#define VIAL_UNLOCK_COMBO_COLS {0,5}
+#define VIAL_UNLOCK_COMBO_ROWS {0,13}
+#define VIAL_UNLOCK_COMBO_COLS {0,3}
 
 #define NO_PRINT
 
